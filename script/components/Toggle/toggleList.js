5abd55a1197b5eab2ae006d8c7c50a1fd42c5354
Лев В. Тарасов <TarasovL@imp.corp>
2025-08-27 17:21:28 +0300

    fix:banner

--- script/components/Toggle/toggleList.js
+++ script/components/Toggle/toggleList.js
@@ -1,14 +1,16 @@
-        function toggleAccordion(id) {
-            const content = document.getElementById(id);
-            const icon = document.querySelector(`[onclick="toggleAccordion('${id}')"] svg`);
+function toggleAccordion(id) {
+    const content = document.getElementById(id);
+    const icon = document.querySelector(`[onclick="toggleAccordion('${id}')"] svg`);
 
-            if (content.classList.contains('max-h-0')) {
-                content.classList.remove('max-h-0', 'opacity-0');
-                content.classList.add('max-h-40', 'opacity-100');
-                icon.classList.add('rotate-180');
-            } else {
-                content.classList.remove('max-h-40', 'opacity-100');
-                content.classList.add('max-h-0', 'opacity-0');
-                icon.classList.remove('rotate-180');
-            }
-        }
+    if (content.classList.contains('max-h-0')) {
+        // Если закрыт — открыть
+        content.classList.remove('max-h-0', 'opacity-0');
+        content.classList.add('max-h-40', 'opacity-100');
+        icon.classList.add('rotate-180');
+    } else {
+        // Если открыт — закрыть
+        content.classList.remove('max-h-40', 'opacity-100');
+        content.classList.add('max-h-0', 'opacity-0');
+        icon.classList.remove('rotate-180');
+    }
+}
